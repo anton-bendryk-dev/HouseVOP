20e2a65d7fb98f22710c742761ba498529585685
anton bendryk <antonbendrik1993@gmail.com>
2021-01-19 18:46:50 +0200

    complete header, added ScrollAnimation

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,13 +3,15 @@ import "./style/App.sass";
 import Header from './header/header';
 import Main from './main/main';
 import Footer from './footer/footer';
-
+import "animate.css/animate.min.css";
 function App() {
   return (
     <div className="wrapper">
+      <div className="inline-wrapper">
         <Header/>
         <Main/>
         <Footer/>
+      </div>
     </div>
   );
 }
--- src/app/header/header.jsx
+++ src/app/header/header.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Logo from '../style/images/HouseVOP.png';
+import ScrollAnimation from 'react-animate-on-scroll';
 const Header = () => {
     return(
         <header>
-            <div className="inline-wrapper">
+            <div className="content-section">
                 <div className="inline-header">
                     <div className="col-w20"><a href="/" className="logo">
-                        <img src={Logo}></img>
+                        <img src={Logo} alt="Logo"></img>
                         <p>ЖИЛОЙ КОМПЛЕКС</p>
                     </a></div>
                     <div className="col-w50 navigation-container">
@@ -16,6 +17,16 @@ const Header = () => {
                         <div className="col-w20"><a href="/">Ипотека</a></div>
                         <div className="col-w20"><a href="/">Контакты</a></div>
                     </div>
+                </div>
+                <div className="title-container">
+                    <h1>Жилой комплекс в историческом центре</h1>
+                    <ScrollAnimation animateIn="animate__fadeInUp">
+                        <span className="mouse"></span>
+                    </ScrollAnimation>
+                </div>
+                <div className="contacts-container">
+                    <div className="address"><span></span>Наб. реки Фонтанки 10-15</div>
+                    <div className="phone"><span></span>8 (812) 123-45-67</div>
                 </div>
             </div>
         </header>
